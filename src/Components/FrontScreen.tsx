9b01a30af0316e14be03d0e08b57495190ac6bc4
Kristína Luptáková <krys.business.mail@gmail.com>
2023-01-31 17:12:42 +0100

    updates 0.2
    adding from old project file

--- src/Components/FrontScreen.tsx
+++ src/Components/FrontScreen.tsx
@@ -17,80 +17,80 @@ import Footer from "./Footer";
 interface FrontScreenProps {}
 
 const FrontScreen = (props: FrontScreenProps) => {
-  const homeRef = createRef<HTMLDivElement>();
-  const handleClick = () => {
-    if (homeRef.current)
-      window.scrollTo({
-        top: homeRef.current.offsetTop,
-        left: 0,
-        behavior: "smooth",
-      });
-  };
-  const Overlay = () => {
-    return <div className={"absolute inset-0 bg-gray-300 bg-opacity-20"} />;
-  };
-  return (
-    <div className="h-full w-full bg-transparent flex flex-col">
-      <div className="FrontScreen relative flex flex-col justify-center items-center grow w-full ">
-        <div className="absolute left-1/7 sm:left-0" ref={homeRef}>
-          <Reservations />
-          <div className="BUTTON fixed bottom-5 right-0 pr-2 lg:pr-4 ">
-            <button
-              onClick={handleClick}
-              type="button"
-              className="lg:h-14 h-8 w-8 lg:w-14 p-2 bg-orange-400 rounded-full"
-            >
-              <ArrowUturnUpIcon className="text-white" />
-            </button>
-          </div>
+    const homeRef = createRef<HTMLDivElement>();
+    const handleClick = () => {
+        if (homeRef.current)
+            window.scrollTo({
+                top: homeRef.current.offsetTop,
+                left: 0,
+                behavior: "smooth",
+            });
+    };
+    const Overlay = () => {
+        return <div className={"absolute inset-0 bg-gray-300 bg-opacity-20"} />;
+    };
+    return (
+        <div className="h-full w-full bg-transparent flex flex-col">
+            <div className="FrontScreen relative flex flex-col justify-center items-center grow w-full ">
+                <div className="absolute left-1/7 sm:left-0" ref={homeRef}>
+                    <Reservations />
+                    <div className="BUTTON fixed bottom-5 right-0 pr-2 lg:pr-4 ">
+                        <button
+                            onClick={handleClick}
+                            type="button"
+                            className="lg:h-14 h-8 w-8 lg:w-14 p-2 bg-orange-400 rounded-full"
+                        >
+                            <ArrowUturnUpIcon className="text-white" />
+                        </button>
+                    </div>
+                </div>
+                <div className={"w-screen h-screen flex flex-col"}>
+                    <Swiper
+                        modules={[Navigation, EffectFade]}
+                        navigation
+                        effect={"slide"}
+                        speed={600}
+                        slidesPerView={1}
+                        loop
+                        className={"w-full h-full"}
+                        pagination={{ clickable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log("slide change")}
+                    >
+                        <SwiperSlide className="w-full">
+                            <Overlay />
+                            <img
+                                src="/assets/images/Placeholder1.jpg"
+                                className="lg:w-full h-[800px] lg:h-full object-cover"
+                                alt={""}
+                            />
+                        </SwiperSlide>{" "}
+                        <SwiperSlide>
+                            <Overlay />
+                            <img
+                                src="/assets/images/ChataBG.jpg"
+                                className=" h-full lg:w-full object-cover "
+                                alt={""}
+                            />
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+            </div>
+            <PageWrapper pageId={"interier"}>
+                <InterierPage />
+            </PageWrapper>
+            <PageWrapper pageId={"exterier"}>
+                <ExterierPage />
+            </PageWrapper>
+            <PageWrapper pageId={"o-nas"}>
+                <AboutPage />
+            </PageWrapper>
+            <PageWrapper pageId={"event"}>
+                <EventsPage />
+            </PageWrapper>
+            <Footer />
         </div>
-        <div className={"w-screen h-screen flex flex-col"}>
-          <Swiper
-            modules={[Navigation, EffectFade]}
-            navigation
-            effect={"slide"}
-            speed={600}
-            slidesPerView={1}
-            loop
-            className={"w-full h-full"}
-            pagination={{ clickable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log("slide change")}
-          >
-            <SwiperSlide className="w-full">
-              <Overlay />
-              <img
-                src="/assets/images/Placeholder1.jpg"
-                className="lg:w-full h-[800px] lg:h-full object-cover"
-                alt={""}
-              />
-            </SwiperSlide>{" "}
-            <SwiperSlide>
-              <Overlay />
-              <img
-                src="/assets/images/ChataBG.jpg"
-                className=" h-full lg:w-full object-cover "
-                alt={""}
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div>
-      <PageWrapper pageId={"interier"}>
-        <InterierPage />
-      </PageWrapper>
-      <PageWrapper pageId={"exterier"}>
-        <ExterierPage />
-      </PageWrapper>
-      <PageWrapper pageId={"o-nas"}>
-        <AboutPage />
-      </PageWrapper>
-      <PageWrapper pageId={"event"}>
-        <EventsPage />
-      </PageWrapper>
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default withRouter(FrontScreen);
--- src/Components/form/TextInput.tsx
+++ src/Components/form/TextInput.tsx
@@ -1,38 +1,38 @@
 import React from "react";
 
 interface TextInputProps {
-  name: any;
-  label: any;
-  placeholder: any;
-  register: any;
-  children?: React.ReactNode | React.ReactNode[];
+    name: any;
+    label: any;
+    placeholder: any;
+    register: any;
+    children?: React.ReactNode | React.ReactNode[];
 }
 
 const TextInput = (props: TextInputProps) => {
-  return (
-    <div className="textInput ">
+    return (
+        <div className="textInput ">
 
-      <label
-        htmlFor="fName"
-        className="mb-1 block text-base text-left  text-white font-medium "
-      >
-        {" "}
-        {props.label}
-      </label>
+            <label
+                htmlFor="fName"
+                className="mb-1 block text-base text-left  text-white font-medium "
+            >
+                {" "}
+                {props.label}
+            </label>
 
-      <input
-        type="text"
-        name={props.name}
-        {...props.register(props.name, { required: true })}
-        required={true}
-        placeholder={props.placeholder}
-        className="w-full rounded-md border placeholder:text-gray-300 bg-white py-3 px-2 text-base lg:text-lg text-[13px] text-left font-medium invalid:border-red-500 outline-none focus:border-orange-400 focus:shadow-md"
-      />
-        <div className={" relative flex flex-row items-end justify-end text-blue-900  font-roboto text-[10px] lg:text-[15px]"}>
-            {props.children}
+            <input
+                type="text"
+                name={props.name}
+                {...props.register(props.name, { required: true })}
+                required={true}
+                placeholder={props.placeholder}
+                className="w-full rounded-md border placeholder:text-gray-300 bg-white py-3 px-2 text-base lg:text-lg text-[13px] text-left font-medium invalid:border-red-500 outline-none focus:border-orange-400 focus:shadow-md"
+            />
+            <div className={" relative flex flex-row items-end justify-end text-blue-900  font-roboto text-[10px] lg:text-[15px]"}>
+                {props.children}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default TextInput;
